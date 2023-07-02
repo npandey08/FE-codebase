c522f17bde6b0410b578c8d6d8bf50c0d36e939e
npandey08 <116940087+npandey08@users.noreply.github.com>
2023-07-02 10:52:33 +0530

    1 more test added

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders h1 link', () => {
   const linkElement = screen.getByText(/Restaurant Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders h1 header', () => {
+  render(<App />);
+  const linkElement2 = screen.getByText(/Railman Caterings/i);
+  expect(linkElement2).toBeInTheDocument();
+});
